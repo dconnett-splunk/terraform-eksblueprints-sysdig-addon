8f9677f547fa993877f4245efcdf7fbb45ae8f33
dconnett-splunk <dconnett@splunk.com>
2022-11-22 12:21:08 -0500

    Update variables.tf

--- blueprints/getting-started/variables.tf
+++ blueprints/getting-started/variables.tf
@@ -2,31 +2,31 @@
 variable "aws_region" {
   description = "AWS target region"
   type        = string
-  default     = "us-east-1"
+  default     = "us-east-2"
 }
 
 # EKS cluster name
 variable "cluster_name" {
   description = "Cluster name"
   type        = string
-  default     = ""
+  default     = "splunk-sysdig-integration-cluster"
 }
 
 # Sysdig Access Key
 variable "sysdig_accesskey" {
   type      = string
-  default   = ""
+  default   = "674bf134-f7e1-426a-a799-1756b76f081f"
   sensitive = true
 }
 
 # Sysdig Collector endpoint url
 variable "sysdig_collector_endpoint" {
   type    = string
-  default = "collector-static.sysdigcloud.com"
+  default = "ingest-us2.app.sysdig.com"
 }
 
 # Sysdig NodeAnalyzer endpoint url
 variable "sysdig_nodeanalyzer_api_endpoint" {
   type    = string
-  default = "secure.sysdig.com"
+  default = "us2.app.sysdig.com"
 }
